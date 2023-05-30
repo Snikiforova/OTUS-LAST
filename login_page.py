5920453a63da1809036e60547608bb00c62479bd
snikiforova <d6796055@gmail.com>
2023-05-30 23:26:44 +0300

    1

---
+++ login_page.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enter_email(self, email):
+        pass
+
+    def enter_password(self, password):
+        pass
+
+    def click_login_button(self):
+        pass
+
+    def check_login_success(self):
+        pass
---
+++ registration_page.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.common.by import By
+
+class RegistrationPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enter_firstname(self, firstname):
+        pass
+
+    def enter_lastname(self, lastname):
+        pass
+
+    def enter_email(self, email):
+        pass
+
+    def enter_telephone(self, telephone):
+        pass
+
+    def enter_password(self, password):
+        pass
+
+    def enter_password_confirm(self, password):
+        pass
+
+    def click_agree(self):
+        pass
+
+    def click_register_button(self):
+        pass
+
+    def check_registration_success(self):
+        pass
