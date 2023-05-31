d91f44edfd92d132f83166de6111bc2ffc87cb36
snikiforova <d6796055@gmail.com>
2023-05-31 14:35:48 +0300

    7772

---
+++ tests/login_page_test.py
@@ -0,0 +1,57 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class LoginPageTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+
+    def tearDown(self):
+
+        self.driver.quit()
+
+    def test_login_page(self):
+        # Тестовый метод
+        driver = self.driver
+        driver.get("https://www.opencart.com/index.php?route=account/login")
+        username = driver.find_element_by_name("email")
+        password = driver.find_element_by_name("password")
+        submit_button = driver.find_element_by_xpath("//input[@type='submit']")
+        username.send_keys("test@example.com")
+        password.send_keys("password123")
+        submit_button.click()
+        self.assertTrue("Account Dashboard" in driver.title)
+
+        class LoginPageTest(unittest.TestCase):
+
+            def setUp(self):
+                firefox_options = webdriver.FirefoxOptions()
+                firefox_options.add_argument("--headless")
+                firefox_options.add_argument("--disable-gpu")
+                self.driver = webdriver.Firefox(firefox_options=firefox_options,
+                                                executable_path="C:\\tools\\geckodriver.exe")
+
+                 Для Edge на Windows 10:
+                self.driver = webdriver.Edge(executable_path="C:\\tools\\MicrosoftWebDriver.exe")
+
+                self.driver.implicitly_wait(10)
+
+            def tearDown(self):
+
+                self.driver.quit()
+
+            def test_login_page(self):
+
+                driver = self.driver
+                driver.get("https://www.opencart.com/index.php?route=account/login")
+                username = driver.find_element_by_name("email")
+                password = driver.find_element_by_name("password")
+                submit_button = driver.find_element_by_xpath("//input[@type='submit']")
+                username.send_keys("test@example.com")
+                password.send_keys("password123")
+                submit_button.click()
+                self.assertTrue("Account Dashboard" in driver.title)
